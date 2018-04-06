0ffa66be3ebed7a0c34dfffc19c9189d9435b182
gilcu3 <828241+gilcu3@users.noreply.github.com>
2018-04-06 14:30:04 -0400

    modified to work with node struct

--- data_structure/segment_tree_beats.cpp
+++ data_structure/segment_tree_beats.cpp
@@ -1,129 +1,127 @@
 /*
 
-	Algorithm:
-		Segment Tree Beats
+    Algorithm:
+        Segment Tree Beats
 
-	Complexity:
-		O(n log(n))
+    Complexity:
+        O(n log(n))
 
-	Tested:
-    	http://acm.hdu.edu.cn/showproblem.php?pid=5306
+    Tested:
+        http://acm.hdu.edu.cn/showproblem.php?pid=5306
 
-	Notes:
-		http://codeforces.com/blog/entry/57319
+    Notes:
+        http://codeforces.com/blog/entry/57319
  */
 
 
 struct segtree_beats{
-	vector<int> mx; // max in node
-	vector<int> mx2; // second (strict) max in node
-	vector<int> mxc; // number of elements equal to mx
-	vector<int> lazy_val;
-	vector<ll> sum;
+    struct node{
+        int mx;      // max in node
+        int mx2;     // second (strict) max in node
+        int mxc;     // number of elements equal to mx
+        int lazy;
+        ll sum;
+    };
+    vector<node> data;
 
-	static constexpr int oo = numeric_limits<int>::max();
+    static constexpr int oo = numeric_limits<int>::max();
 
-	void init(int n, const vector<int>& A){
-		int size = 1  << (33-__builtin_clz(n));
-		mx.resize(size);
-		mx2.resize(size);
-		mxc.resize(size);
-		lazy_val.resize(size);
-		sum.resize(size);
+    void init(const vector<int>& A){
+        int n = A.size();
+        data.resize(1  << (33-__builtin_clz(n)));
+        build(1, 0, n, A);
+    }
 
-		build(1, 0, n, A);
-	}
+    void update_vals(node& nv, const node& left, const node& right){
+        nv.mx = max(left.mx, right.mx);
+        nv.mx2 = max(left.mx2, right.mx2);
+        nv.mxc = 0;
+        
+        if(left.mx == nv.mx) nv.mxc += left.mxc;
+        else nv.mx2 = max(nv.mx2, left.mx);
+            
+        if(right.mx == nv.mx) nv.mxc += right.mxc;
+        else nv.mx2 = max(nv.mx2, right.mx);
+        nv.sum = left.sum + right.sum;
 
-	void update_vals(int nod){
-		mx[nod] = max(mx[(nod << 1)], mx[(nod << 1)+1]);
-		mxc[nod] = (mx[nod] == mx[(nod << 1)] ? mxc[nod << 1] : 0) + (mx[nod] == mx[(nod << 1) + 1] ? mxc[(nod << 1) + 1] : 0);
+        nv.lazy = oo;
+    }
 
-		if(mx[(nod << 1)] == mx[(nod << 1)+1]) mx2[nod] = max(mx2[(nod << 1)], mx2[(nod << 1)+1]);
-		else if(mx[(nod << 1)] < mx[(nod << 1)+1]) mx2[nod] =  max(mx[nod << 1], mx2[(nod << 1) + 1]);
-		else mx2[nod] = max(mx[(nod << 1) + 1], mx2[(nod << 1) ]);
+    void build( int p, int b, int e, const vector<int>& A){
+        if( b + 1 == e ){
+            data[p] = node{A[b], -1, 1, oo, A[b]};
+            return;
+        }
+        int m = (b + e) >> 1;
 
-		sum[nod] = sum[(nod << 1)] + sum[(nod << 1)+1];
-	}
+        build(p << 1,  b, m, A);
+        build((p << 1) + 1,  m, e, A);
 
-	void build( int p, int b, int e, const vector<int>& A){
-	    if( b + 1 == e ){
-	        mx[p] = A[b];
-	        mx2[p] = -1;
-	        mxc[p] = 1;
-	        sum[p] = A[b];
-	        lazy_val[p] = oo;
-	        return;
-	    }
-	    int m = (b+e)>>1;
+        update_vals(data[p], data[p << 1], data[(p << 1) + 1]);
+    }
 
-	    build(p << 1,  b, m, A);
-	    build((p << 1) + 1,  m, e, A);
-
-	    update_vals(p);
-
-	    lazy_val[p] = oo;
-	}
-
-	void puttag(int p, int c){
-		//assert(mx2[p] < c);
-	    sum[p] -= (ll)(mx[p] - c) * mxc[p];
-	    mx[p] = c;
-	    lazy_val[p] = c;
-	}
-	void pushdown(int p ){
-		if(mx[p << 1] > lazy_val[p]) puttag(p << 1, lazy_val[p]);
-		if(mx[(p << 1) + 1] > lazy_val[p]) puttag(1 + (p << 1), lazy_val[p]);
-		lazy_val[p] = oo;
-	}
+    void puttag(node& cur, int c){
+        //assert(data[p].mx2 < c);
+        cur.sum -= (ll)(cur.mx - c) * cur.mxc;
+        cur.mx = c;
+        cur.lazy = c;
+    }
+    void pushdown( int p){
+        node& cur = data[p];
+        if(data[p << 1].mx > cur.lazy) puttag(data[p << 1], cur.lazy);
+        if(data[(p << 1) + 1].mx > cur.lazy) puttag(data[1 + (p << 1)], cur.lazy);
+        cur.lazy = oo;
+    }
 
 
-	void update_min(int p,  int b, int e, int bb, int ee, int c ){
-	    if( mx[p] <= c ) return; // break_condition()
+    void update_min( int p, int b, int e, int bb, int ee, int c ){
 
-	    if( b + 1 == e ){ // leaf
-	            mx[p] = c;
-	            sum[p] = c;
-	            return;
-	    }
+        if( data[p].mx <= c ) return; // break_condition()
 
-	    if( bb <= b && e <= ee && mx2[p] < c){ // tag_condition()
-	    	puttag(p, c);
-	        return;
-	    }
+        if( b + 1 == e ){ // leaf
+                data[p].mx = c;
+                data[p].sum = c;
+                return;
+        }
 
-	    if( lazy_val[p] != oo ) pushdown(p );
+        if( bb <= b && e <= ee && data[p].mx2 < c){ // tag_condition()
+            puttag(data[p], c);
+            return;
+        }
 
-	    int m = (b+e)>>1;
-	    if(bb < m)update_min( p << 1, b, m, bb, ee, c );
-	    if(m < ee)update_min( 1 + (p << 1), m, e, bb, ee, c );
+        if( data[p].lazy != oo ) pushdown(p);
 
-	    update_vals(p);
-	}
+        int m = ( b + e ) >> 1;
+        if(bb < m)update_min( p << 1, b, m , bb, ee, c);
+        if(m < ee)update_min( (p << 1)+1, m, e, bb, ee, c);
+        update_vals(data[p], data[p << 1], data[(p << 1) + 1]);
+    }
 
 
-	int query_max( int p, int b, int e, int bb, int ee ){
+    int query_max( int p, int b, int e, int bb, int ee ){
 
-	    if( bb <= b && e <= ee ) return mx[p];
+        if( bb <= b && e <= ee ) return data[p].mx;
 
-	    if( lazy_val[p] != oo ) pushdown(p );
+        if( data[p].lazy != oo ) pushdown(p);
 
-	    int m = (b+e)>>1;
-	    int ans = 0;
-	    if(bb < m)ans = max(ans, query_max(p << 1,  b, m, bb, ee ));
-	    if(m < ee)ans = max(ans, query_max( 1 + (p << 1), m, e,  bb, ee ));
-	    return ans;
-	}
+        int ans = 0, m = (b + e) >> 1;
+        if(bb < m) ans = max(ans, query_max( p << 1, b, m, bb, ee));
+        if(m < ee) ans = max(ans, query_max( (p << 1)+1, m, e , bb, ee) );
 
-	ll query_sum( int p, int b, int e, int bb, int ee ){
+        return ans;
+    }
 
-		if( bb <= b && e <= ee ) return sum[p];
+    ll query_sum( int p, int b, int e, int bb, int ee ){
 
-		if( lazy_val[p] != oo ) pushdown( p);
+        if( bb <= b && e <= ee ) return data[p].sum;
 
-		int m = (b+e)>>1;
-		ll ans = 0;
-		if(bb < m)ans += query_sum( p << 1, b, m, bb, ee );
-		if(m < ee)ans += query_sum( 1 + (p << 1), m, e, bb, ee );
-		return ans;
-	}
+        if( data[p].lazy != oo ) pushdown(p);
+
+        ll ans = 0;
+        int m = (b + e) >> 1;
+        if(bb < m) ans += query_sum( p << 1, b, m, bb, ee);
+        if(m < ee) ans += query_sum( (p << 1)+1, m, e , bb, ee);
+
+        return ans;
+    }
 };
